7ba96035aa70e6f7f322f3b4c7280e5a55b56c57
Juan J Malaver <juanmagri@outlook.es>
2025-09-18 19:12:34 -0500

    Validations functions and styles added for search input

--- src/components/molecules/SearchBar.tsx
+++ src/components/molecules/SearchBar.tsx
@@ -9,6 +9,7 @@ import '@/styles/molecules/SearchBar.css';
 import SortModal from '../molecules/SortModal';
 import { Link } from 'react-router-dom';
 import FavoriteButton from '../atoms/FavoriteButton';
+import Text from '../atoms/Text';
 
 interface SearchBarProps {
    sortBy?: 'number' | 'name';
@@ -16,18 +17,57 @@ interface SearchBarProps {
 }
 
 const SearchBar = ({ sortBy, onSortChange }: SearchBarProps) => {
+   const [keyword, setKeyword] = useState('');
+   const [error, setError] = useState('');
    const [showSort, setShowSort] = useState(false);
    const location = useLocation();
+
+   const validateKeyword = (e: React.ChangeEvent<HTMLInputElement>) => {
+      const value = e.target.value;
+      setKeyword(value);
+
+      const trimmed = value.trim();
+
+      if (trimmed.length === 0 || trimmed.length === 1) {
+         setError('');
+         return;
+      }
+
+      if (trimmed.length < 3) {
+         setError('Enter at least 3 characters.');
+      } else if (trimmed.length > 30) {
+         setError('Search term is too long.');
+      } else if (!/^[a-zA-Z\s-]+$/.test(trimmed)) {
+         setError('Only letters, spaces, and hyphens are allowed.');
+      } else {
+         setError('');
+      }
+   };
    console.log(location);
 
    return (
       <div className="search-bar">
-         <Input
-            placeholder="Search"
-            id="search"
-            name="search"
-            className="search-bar__input"
-         />
+         <div
+            className={`search-bar__wrapper ${
+               error ? 'search-bar__wrapper--error' : ''
+            }`}
+         >
+            <Input
+               placeholder="Search"
+               id="search"
+               name="search"
+               className="search-bar__input"
+               value={keyword}
+               onChange={(e) => {
+                  validateKeyword(e);
+               }}
+            />
+            {error && (
+               <Text as="p" className="search-bar__error">
+                  {error}
+               </Text>
+            )}
+         </div>
          {location.pathname !== '/favorites' && (
             <Button
                onClick={() => setShowSort((prev) => !prev)}
